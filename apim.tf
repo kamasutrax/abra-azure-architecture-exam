36061a12bfb7bf328ec56dce5d7caa828d100fa7
DustK8s <47569446+kamasutrax@users.noreply.github.com>
2026-05-27 09:07:03 +0300

    Update apim.tf

--- apim.tf
+++ apim.tf
@@ -1,13 +1,24 @@
-# API Management
-resource "azurerm_api_management" "apim" {
-  name                = "apim-exam-internal-gw"
+# Public IP required for APIM in External VNet mode
+resource "azurerm_public_ip" "apim_pip" {
+  name                = "pip-apim-exam"
   location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
-  publisher_name      = "Exam Org"
-  publisher_email     = "admin@exam.org"
-  sku_name            = "Developer_1"
+  allocation_method   = "Static"
+  sku                 = "Standard"
+  zones               = ["1", "2"]
+}
 
-  virtual_network_type = "Internal"
+# API Management deployed in External mode for user ingress
+resource "azurerm_api_management" "apim" {
+  name                 = "apim-exam-gw"
+  location             = azurerm_resource_group.rg.location
+  resource_group_name  = azurerm_resource_group.rg.name
+  publisher_name       = "Exam Org"
+  publisher_email      = "admin@exam.org"
+  sku_name             = "Developer_1"
+  
+  virtual_network_type = "External"
+  public_ip_address_id = azurerm_public_ip.apim_pip.id
 
   virtual_network_configuration {
     subnet_id = azurerm_subnet.private_apim.id
@@ -16,6 +27,7 @@ resource "azurerm_api_management" "apim" {
   zones = ["1", "2"]
 }
 
+# API configuration routing traffic to the Application Gateway, enforcing HTTPS
 resource "azurerm_api_management_api" "api" {
   name                = "hello-api"
   resource_group_name = azurerm_resource_group.rg.name
@@ -23,6 +35,6 @@ resource "azurerm_api_management_api" "api" {
   revision            = "1"
   display_name        = "Hello World API"
   path                = "hello"
-  protocols           = ["https", "http"]
+  protocols           = ["https"]
   service_url         = "http://10.0.4.10"
 }
